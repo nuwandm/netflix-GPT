a894a638287409246339667ad0748c18a2257639
Nuwan Madusanka <nuwandarshana2012@gmail.com>
2024-04-25 08:25:19 +0530

    addod logo and created routes

---
+++ src/components/Login.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+
+const Login = () => {
+	return (
+		<div>
+			<Header />
+			<img
+				src="https://assets.nflxext.com/ffe/siteui/vlv3/9f46b569-aff7-4975-9b8e-3212e4637f16/c48bf50e-ab17-45aa-8a65-e49a2e1a6f5d/LK-en-20240415-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+				alt="Netflix-Logo"
+			/>
+		</div>
+	);
+};
+
+export default Login;
