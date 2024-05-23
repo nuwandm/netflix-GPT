beb2ac1695c57a4a22bd58acc5bdc96e9e3df2cf
Nuwan Madusanka <nuwandarshana2012@gmail.com>
2024-05-23 14:52:50 +0530

    prevent unnecessary API calls

--- src/hooks/useNowPlayingMoviesList.js
+++ src/hooks/useNowPlayingMoviesList.js
@@ -1,4 +1,4 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { API_OPTIONS } from "../utils/constants";
 import { addNowPlayingMovies } from "../utils/moviesSlice";
 import { useEffect } from "react";
@@ -6,12 +6,16 @@ import { useEffect } from "react";
 const useNowPlayingMovies = () => {
 	const dispatch = useDispatch();
 
+	const nowPlayingMovies = useSelector(
+		(store) => store.movies.nowPlayingMovies
+	);
+
 	const getNowPlayingMovies = async () => {
 		const data = await fetch(
 			"https://api.themoviedb.org/3/movie/now_playing?page=1",
 			API_OPTIONS
 		);
-		
+
 		const json = await data.json();
 
 		dispatch(addNowPlayingMovies(json.results));
@@ -19,7 +23,7 @@ const useNowPlayingMovies = () => {
 	};
 
 	useEffect(() => {
-		getNowPlayingMovies();
+		if (!nowPlayingMovies) getNowPlayingMovies();
 	}, []);
 };
 
